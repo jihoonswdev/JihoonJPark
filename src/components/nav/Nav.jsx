75eb6394af932763e52987427bed434b03661a27
Jason Park <jihoonswdev@gmail.com>
2022-05-03 15:51:26 -0400

    update nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,12 +5,14 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {RiCustomerService2Fill} from 'react-icons/ri'
 import {BiMessageAltDetail} from 'react-icons/bi'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#home"><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
+      <a href="#root" className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#experience"><BiBook/></a>
       <a href="#services"><RiCustomerService2Fill/></a>
       <a href="#contact"><BiMessageAltDetail/></a>
