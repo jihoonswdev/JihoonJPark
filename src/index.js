e12e47febd75bb9a635a6ff7d96da2f337de5e75
Jason Park <jihoonswdev@gmail.com>
2022-05-03 13:59:38 -0400

    upgrade to react 18

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
 import App from './App'
 import './index.css'
 
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(<App />);
 
+// import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('App');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App tab="home" />);
 
-
-ReactDom.render(<App/>, document.querySelector('#root'));
+// ReactDom.render(<App/>, document.querySelector('#root'));
