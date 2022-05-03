d33d89a08c8059c489bf81ad6be8faa10bdd3b02
Jason Park <jihoonswdev@gmail.com>
2022-05-03 13:24:24 -0400

    update changes

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import CV from '../../assets/Ji Hoon Park Resume.pdf'
-import ME from '../../assets/Me1.png'
+// import ME from '../../assets/Me2.png'
 
 function CTA() {
   return (
@@ -8,11 +8,7 @@ function CTA() {
         <a href={CV} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
 
-        <div className="me">
-            <img src={ME} alt="" />
-        </div>
-
-        <a href="#contact" className='scroll__down'>Scroll Down</a>
+        
     </div>
     
   )
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
 
-function HeaderSocials() {
+const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/jasonjihoonpark/" target='_blank'><BsLinkedin /></a>
-        <a href="https://github.com/jihoonswdev" target='_blank'><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/jasonjihoonpark/" target='__blank'><BsLinkedin /></a>
+        <a href="https://github.com/jihoonswdev" target='__blank'><FaGithub /></a>
     </div>
   )
 }
