29fda077e808b913efc9f351307e591d1e58f076
Jason Park <jihoonswdev@gmail.com>
2022-05-04 18:57:21 -0400

    update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,24 +13,24 @@ function Contact() {
       <div className="container contact__container">
         <div className="container__options">
           <article className="contact__option">
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
-            <h4>jihoonswdev@gmail.com</h4>
-            <a href="mailto:email@gmail.com" target='__blank'>Send a Message</a>
+            {/* <h5>jihoonswdev@gmail.com</h5> */}
+            <a href="mailto:jihoonswdev@gmail.com" target='__blank'>Send a Message</a>
           </article>
 
           <article className="contact__option">
-          <RiMessengerLine/>
+          <RiMessengerLine className="contact__option-icon"/>
             <h4>Messenger</h4>
-            <h4>email@gmail.com</h4>
+            {/* <h5>email@gmail.com</h5> */}
             <a href="https://m.me/gentlegiant312" target='__blank'>Send a Message</a>
           </article>
 
           <article className="contact__option">
-          <BsWhatsapp/>
+          <BsWhatsapp className="contact__option-icon"/>
             <h4>WhatsApp</h4>
-            <h3>+15512656088</h3>
-            <a href="https://api.whatsapp.com/send?phone=5512656088"target='__blank'>Send a Message</a>
+            {/* <h5>+15512656088</h5> */}
+            <a href="https://api.whatsapp.com/send?phone=5512656088" target='__blank'>Send a Message</a>
           </article>
 
         </div>
