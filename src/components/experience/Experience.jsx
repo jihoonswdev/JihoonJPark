45bc1128f46c71ecaadac20b7d6923f4f8f16f73
Jason Park <jihoonswdev@gmail.com>
2022-05-04 14:17:07 -0400

    update experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,112 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skill I have</h5>
+      <h2>My Experience</h2>
+
+      {/* FrontEnd */}
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>CSS</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>JavaScript</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>APIs</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>React</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>Bootstrap</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+          </div>
+        </div>
+
+          {/* Backend */}
+        <div className="experience__backend">
+        <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>Node.js</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>Express</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>MongoDB</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>APIs</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>React</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>Bootstrap</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+          </div>
+        </div>
+
+        {/* Database */}
+        <div className="experience__database">
+        <h3>Database & More</h3>
+          <div className="experience__content">
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>MySQL</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>Git</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__detail'>
+              <BsPatchCheckFill/>
+              <h4>Heroku</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+          </div>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
