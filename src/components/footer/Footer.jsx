3341f7b853a93f2c36d8a136b7b4c7fbd11b2ecf
Jason Park <jihoonswdev@gmail.com>
2022-05-04 22:05:08 -0400

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Jason Park</a>
+
+      <ul className='permalinks'></ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#contact">Contact</a></li>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><FaFacebookF/></a>
+        <a href="https://instagram.com"><FiInstagram/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Jason Park. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
