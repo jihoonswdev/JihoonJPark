7e08e5aa074a5725112a82a08df4def6bf213dff
Jason Park <jihoonswdev@gmail.com>
2022-05-05 10:20:04 -0400

    finished experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,118 +1,106 @@
 import React from 'react'
 import './experience.css'
-import {BsPatchCheckFill} from 'react-icons/bs'
+import {AiOutlineHtml5} from 'react-icons/ai'
+import {IoLogoCss3} from 'react-icons/io'
+import {SiJavascript} from 'react-icons/si'
+import {AiOutlineApi} from 'react-icons/ai'
+import {FaReact} from 'react-icons/fa'
+import {FaBootstrap} from 'react-icons/fa'
+import {FaNodeJs} from 'react-icons/fa'
+import {SiExpress} from 'react-icons/si'
+import {SiMongodb} from 'react-icons/si'
+import {SiMysql} from 'react-icons/si'
+import {BsGithub} from 'react-icons/bs'
+import {FaGit} from 'react-icons/fa'
+import {SiHeroku} from 'react-icons/si'
 
 const Experience = () => {
   return (
     <section id='experience'>
-      <h5>What Skill I have</h5>
-      <h2>My Experience</h2>
+      <h5 className='h5'>What Skills I have</h5>
+      <h2>My Experiences</h2>
 
-      {/* FrontEnd */}
+                                    {/* FrontEnd */}
       <div className="container experience__container">
         <div className="experience__frontend">
-          <h3>Frontend Development</h3>
+          <h3 className='h3'>Frontend Development</h3>
           <div className="experience__content">
 
             <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <AiOutlineHtml5 className='experience__details-icon'/>
               <div>
-                <h4>HTML</h4>
+                <h4 className='h4'>HTML</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
 
             <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <IoLogoCss3 className='experience__details-icon'/>
               <div>
-                <h4>CSS</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/> 
-              <div>
-                <h4>JavaScript</h4>
+                <h4 className='h4'>CSS</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
 
             <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <SiJavascript className='experience__details-icon'/> 
               <div>
-                <h4>APIs</h4>
+                <h4 className='h4'>JavaScript</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
 
             <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <AiOutlineApi className='experience__details-icon'/>
               <div>
-                <h4>React</h4>
+                <h4 className='h4'>APIs</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
 
             <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <FaReact className='experience__details-icon'/>
               <div>
-                <h4>Bootstrap</h4>
+                <h4 className='h4'>React</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <FaBootstrap className='experience__details-icon'/>
+              <div>
+                <h4 className='h4'>Bootstrap</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
           </div>
         </div>
 
-          {/* Backend */}
+                                    {/* Backend */}
         <div className="experience__backend">
-        <h3>Backend Development</h3>
+        <h3 className='h3'>Backend Development</h3>
           <div className="experience__content">
 
             <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <FaNodeJs className='experience__details-icon'/>
               <div>
-                <h4>Node.js</h4>
+                <h4 className='h4'>Node.js</h4>
                 <small className='text-light'>Experienced</small>
               </div>
-              
             </article>
+
             <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <SiExpress className='experience__details-icon'/>
               <div>
-                <h4>Express</h4>
+                <h4 className='h4'>Express</h4>
                 <small className='text-light'>Experienced</small>
               </div>
-              
             </article>
+
             <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <SiMongodb className='experience__details-icon'/>
               <div>
-                <h4>MongoDB</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-              
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>APIs</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-              
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>React</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-              
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Bootstrap</h4>
+                <h4 className='h4'>MongoDB</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
@@ -120,31 +108,39 @@ const Experience = () => {
           </div>
         </div>
 
-        {/* Database */}
+                                    {/* Database */}
         <div className="experience__database">
-        <h3>Database & More</h3>
+        <h3 className='h3'>Database & More</h3>
           <div className="experience__content">
 
             <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <SiMysql className='experience__details-icon'/>
               <div>
-                <h4>MySQL</h4>
+                <h4 className='h4'>MySQL</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
 
             <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <BsGithub className='experience__details-icon'/>
               <div>
-                <h4>Git</h4>
+                <h4 className='h4'>Github</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
 
             <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <FaGit className='experience__details-icon'/>
               <div>
-                <h4>Heroku</h4>
+                <h4 className='h4'>Git</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <SiHeroku className='experience__details-icon'/>
+              <div>
+                <h4 className='h4'>Heroku</h4>
               <small className='text-light'>Experienced</small>
               </div>
             </article>
